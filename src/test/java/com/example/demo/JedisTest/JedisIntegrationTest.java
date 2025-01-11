0e55ecfe98408edb6023785e92199a4a9bacdcb6
lleee123 <brightlee111993@gmail.com>
2025-01-11 18:01:23 +0800

    update Jedis test

--- src/test/java/com/example/demo/JedisTest/JedisIntegrationTest.java
+++ src/test/java/com/example/demo/JedisTest/JedisIntegrationTest.java
@@ -9,7 +9,10 @@ public class JedisIntegrationTest {
 
     @Test
     void shouldStoreAndRetrieveValueFromRedis() {
-        try (Jedis jedis = new Jedis("localhost", 6379)) { // 修改為你的 Redis 伺服器地址與端口
+        try (
+                Jedis jedis = new Jedis("redis", 6379)
+//                Jedis jedis = new Jedis("localhost", 6379)
+        ) { // 修改為你的 Redis 伺服器地址與端口
             // 如果需要密碼驗證，請提供密碼
             jedis.auth("123456");
 
