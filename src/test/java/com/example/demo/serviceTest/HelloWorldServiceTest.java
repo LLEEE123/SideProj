24f9ad07f1bdfe7084315cd8d9e3a2240eb07d4b
lleee123 <brightlee111993@gmail.com>
2025-01-15 14:51:14 +0800

    update controller for temp

--- src/test/java/com/example/demo/serviceTest/HelloWorldServiceTest.java
+++ src/test/java/com/example/demo/serviceTest/HelloWorldServiceTest.java
@@ -17,6 +17,6 @@ public class HelloWorldServiceTest {
         String result = helloWorldService.sayHello();
 
         // Assert
-        assert(result.equals("Hello World Testing for local!"));
+        assert(result.equals("Hello World Testing for Aws!"));
     }
 }
